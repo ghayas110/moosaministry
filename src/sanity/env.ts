b39324c3139c60f74288719d052938981db7eca5
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2026-04-27 19:22:27 +0500

    aa

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,14 +1,14 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-01";
+  process.env.NEXT_PUBLIC_SANITYS_API_VERSION || "2024-10-01";
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing env: NEXT_PUBLIC_SANITY_DATASET"
+  process.env.NEXT_PUBLIC_SANITYS_DATASET,
+  "Missing env: NEXT_PUBLIC_SANITYS_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing env: NEXT_PUBLIC_SANITY_PROJECT_ID"
+  process.env.NEXT_PUBLIC_SANITYS_PROJECT_ID,
+  "Missing env: NEXT_PUBLIC_SANITYS_PROJECT_ID"
 );
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
